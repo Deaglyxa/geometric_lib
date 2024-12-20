36cb278e8e46e6b1d0a959c33c457f01318a75a3
Konstantin Stakhurskiy <130889531+Deaglyxa@users.noreply.github.com>
2024-12-20 23:55:38 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -2,9 +2,12 @@ import math
 
 
 def area(r):
+    if radius < 0:
+        raise ValueError("Radius cannot be negative")
     return math.pi * r * r
 
 
 def perimeter(r):
+    if radius < 0:
+        raise ValueError("Radius cannot be negative")
     return 2 * math.pi * r
-
